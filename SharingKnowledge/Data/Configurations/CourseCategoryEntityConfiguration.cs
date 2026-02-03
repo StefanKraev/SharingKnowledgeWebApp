6f2f8d5ba748aed419285565bbcba1337b707cb8
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 20:27:02 +0200

    Configuring model CourseCategory in database and adding seed data

---
+++ SharingKnowledge/Data/Configurations/CourseCategoryEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SharingKnowledge.Models;
+
+namespace SharingKnowledge.Data.Configurations
+{
+    public class CourseCategoryEntityConfiguration : IEntityTypeConfiguration<CourseCategory>
+    {
+        private readonly ICollection<CourseCategory> categories = new List<CourseCategory>
+        {
+            new CourseCategory { Id = 1, Name = "Programming" },
+            new CourseCategory { Id = 2, Name = "Databases" },
+            new CourseCategory { Id = 3, Name = "Web Development" },
+            new CourseCategory { Id = 4, Name = "Data Science" },
+            new CourseCategory { Id = 5, Name = "Mobile Development" },
+            new CourseCategory { Id = 6, Name = "Cloud Computing" },
+            new CourseCategory { Id = 7, Name = "DevOps" },
+            new CourseCategory { Id = 8, Name = "Cybersecurity" },
+            new CourseCategory { Id = 9, Name = "AI & Machine Learning" },
+            new CourseCategory { Id = 10, Name = "Game Development" }
+        };
+
+        public void Configure(EntityTypeBuilder<CourseCategory> entity)
+        {
+            entity.HasData(categories);
+        }
+    }
+}
