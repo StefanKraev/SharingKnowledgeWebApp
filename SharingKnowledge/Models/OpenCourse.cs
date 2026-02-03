8eb194d31b50bc10f3b968061dee29139be10ff0
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 19:15:28 +0200

    Add properties to model OpenCourse

--- SharingKnowledge/Models/OpenCourse.cs
+++ SharingKnowledge/Models/OpenCourse.cs
@@ -1,6 +1,23 @@
-﻿namespace SharingKnowledge.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharingKnowledge.Models
 {
     public class OpenCourse
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+
     }
 }
