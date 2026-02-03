3af1beb9fb4a4d128acb1cfe28ec4ec683385502
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 21:23:30 +0200

    Created dummy view for configuring courses displaying

--- SharingKnowledge/Controllers/OpenCoursesController.cs
+++ SharingKnowledge/Controllers/OpenCoursesController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using SharingKnowledge.Data;
 using SharingKnowledge.Models;
+using SharingKnowledge.ViewModels;
 
 namespace SharingKnowledge.Controllers
 {
@@ -22,7 +23,18 @@ namespace SharingKnowledge.Controllers
                 .Include(oc => oc.Category)
                 .ToList();
 
-            return View();
+            IEnumerable<OpenCoursesAllViewModel> coursesAllViewModels = openCourses
+                .Select(oc => new OpenCoursesAllViewModel
+                {
+                    Title = oc.Title,
+                    Description = oc.Description,
+                    StartDate = oc.StartDate,
+                    ImageUrl = oc.ImageUrl,
+                    CategoryName = oc.Category.Name
+                })
+                .ToList();
+
+            return View(coursesAllViewModels);
         }
     }
 }
