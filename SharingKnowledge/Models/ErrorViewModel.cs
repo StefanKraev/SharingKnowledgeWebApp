93c124a52a099605d7913ed011bd14f9ff3a157b
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 18:45:30 +0200

    Add project files.

---
+++ SharingKnowledge/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SharingKnowledge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
