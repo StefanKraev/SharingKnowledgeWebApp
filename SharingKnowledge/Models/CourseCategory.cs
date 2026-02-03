61840e86fd2818d33d4a48d6a6740f3f9c7bd5a4
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 19:26:28 +0200

    Adding poperties to model "CourseCategory"

--- SharingKnowledge/Models/CourseCategory.cs
+++ SharingKnowledge/Models/CourseCategory.cs
@@ -1,6 +1,15 @@
-﻿namespace SharingKnowledge.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharingKnowledge.Models
 {
     public class CourseCategory
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<OpenCourse> Courses { get; set; } = new List<OpenCourse>();
     }
 }
