a6c0c4435f76275cdb1c6a5556fa14c96cc3559a
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 21:17:16 +0200

    OpenCourses ViewModel created for displaying all courses

--- SharingKnowledge/Controllers/OpenCoursesController.cs
+++ SharingKnowledge/Controllers/OpenCoursesController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using SharingKnowledge.Data;
+using SharingKnowledge.Models;
 
 namespace SharingKnowledge.Controllers
 {
     public class OpenCoursesController : Controller
     {
+        private readonly ApplicationDbContext DbContext;
+        public OpenCoursesController(ApplicationDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-
+            IEnumerable<OpenCourse> openCourses = DbContext
+                .OpenCourses
+                .AsNoTracking()
+                .Include(oc => oc.Category)
+                .ToList();
 
             return View();
         }
--- SharingKnowledge/ViewModels/OpenCoursesAllViewModel.cs
+++ SharingKnowledge/ViewModels/OpenCoursesAllViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace SharingKnowledge.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharingKnowledge.ViewModels
 {
     public class OpenCoursesAllViewModel
     {
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        [Required]
+
+        public string CategoryName { get; set; } = null!;
     }
 }
