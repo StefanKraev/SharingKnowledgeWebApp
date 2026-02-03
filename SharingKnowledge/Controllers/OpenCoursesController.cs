b4e4c5d343ca7fc396c93913c2bb87aed636987e
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 21:05:12 +0200

    Created new Controller "OpenCoursesController"

---
+++ SharingKnowledge/Controllers/OpenCoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharingKnowledge.Controllers
+{
+    public class OpenCoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
