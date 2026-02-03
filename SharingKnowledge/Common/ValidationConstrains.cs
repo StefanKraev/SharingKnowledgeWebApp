24e778c098a6acbe78f5914a29a06fe6c7b64d3a
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 19:52:37 +0200

    Added Validations for model properties

--- SharingKnowledge/Common/ValidationConstrains.cs
+++ SharingKnowledge/Common/ValidationConstrains.cs
@@ -2,5 +2,16 @@
 {
     public class ValidationConstrains
     {
+        /*OpenCourse property validation*/
+        public const int CourseTitleMinLength = 2;
+        public const int CourseTitleMaxLength = 100;
+        public const int CourseDescriptionMinLength = 10;
+        public const int CourseDescriptionMaxLength = 2000;
+        public const string CourseImageUrlRegularExpression = @"^(http|https):\/\/[^\s$.?#].[^\s]*$";
+        public const int CourseImageUrlMaxLength = 2048;
+
+        /*CourseCategory property validations*/
+        public const int CategoryNameMinLength = 2;
+        public const int CategoryNameMaxLength = 100;
     }
 }
