2ca5d8b3323e78d3bb28afbd6f66291dc1aae16a
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-03 21:44:12 +0200

    Created OpenCourse ViewModel for a single course for Details

---
+++ SharingKnowledge/ViewModels/Courses/OpenCoursesDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharingKnowledge.ViewModels.Courses
+{
+    public class OpenCoursesDetailsViewModel
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        [Required]
+
+        public string CategoryName { get; set; } = null!;
+    }
+}
