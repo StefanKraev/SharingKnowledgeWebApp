5b033348eadfbf68fe58a9ff334c6f5a9a5ac4e6
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-04 12:30:43 +0200

    Created Input Model with validations for OpenCourse validation

--- SharingKnowledge/ViewModels/Courses/OpenCoursesCreateInputModel.cs
+++ SharingKnowledge/ViewModels/Courses/OpenCoursesCreateInputModel.cs
@@ -1,6 +1,24 @@
-﻿namespace SharingKnowledge.ViewModels.Courses
+﻿using System.ComponentModel.DataAnnotations;
+using static SharingKnowledge.Common.ValidationConstrains;
+
+namespace SharingKnowledge.ViewModels.Courses
 {
     public class OpenCoursesCreateInputModel
     {
+        [Required]
+        [MinLength(CourseTitleMinLength)]
+        [MaxLength(CourseTitleMaxLength)]
+        public string Title { get; set; } = null!;
+        [Required]
+        [MinLength(CourseDescriptionMinLength)]
+        [MaxLength(CourseDescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+        [Required]
+        public DateTime StartDate { get; set; }
+        [RegularExpression(CourseImageUrlRegularExpression)]
+        [MaxLength(CourseImageUrlMaxLength)]
+        public string ?ImageUrl { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
     }
 }
