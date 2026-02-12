9716546a757572dea4abf83d497b25eac794fa59
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-12 18:30:49 +0200

    Added configuration for Student model

---
+++ SharingKnowledge/Data/Configurations/StudentEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SharingKnowledge.Models;
+
+namespace SharingKnowledge.Data.Configurations
+{
+    public class StudentEntityConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> entity)
+        {
+            entity
+                .HasIndex(s => s.FacultyNumber) //for fast searching of student.
+                .IsUnique(); //cannot be written as an attribute [key] as it will
+                             //be in conflict with IdentitiyUser::id
+
+        }
+    }
+}
