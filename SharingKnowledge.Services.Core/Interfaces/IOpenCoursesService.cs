53e78fb297114e93a2f9d4639f4d029edb209c50
StefanKraev <stefanemilovkraev@abv.bg>
2026-02-14 15:41:15 +0200

    Added essential methos/tasks in IService. To be implemented in Service

--- SharingKnowledge.Services.Core/Interfaces/IOpenCoursesService.cs
+++ SharingKnowledge.Services.Core/Interfaces/IOpenCoursesService.cs
@@ -1,9 +1,27 @@
-﻿using SharingKnowledge.ViewModels.Courses;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using SharingKnowledge.ViewModels.Courses;
+using SharingKnowledge.Models;
 
 namespace SharingKnowledge.Services.Core.Interfaces
 {
     public interface IOpenCoursesService
     {
-        Task<IEnumerable<OpenCoursesAllViewModel>> GetAllOpenCoursesAsync();
+        Task<IEnumerable<OpenCoursesAllViewModel>> GetAllCoursesAsync(); //all
+
+        Task<OpenCoursesDetailsViewModel?> GetCourseDetailsAsync(int id); //details
+
+        Task CreateCourseAsync(OpenCoursesCreateInputModel inputModel, string userId); //create
+
+        Task<OpenCoursesCreateInputModel?> GetCourseForEditAsync(int id, string userId); //edit
+
+        Task<bool> EditCourseAsync(int id, OpenCoursesCreateInputModel inputModel, string userId); //edit
+
+        Task<OpenCoursesDeleteViewModel?> GetCourseForDeleteAsync(int id, string userId); //delete
+
+        Task<bool> DeleteCourseAsync(int id, string userId); // delete
+
+        Task<IEnumerable<CourseCategory>> GetCategoriesAsync(); // getCategories
+
+        Task<bool> CategoryExistsAsync(int categoryId); //existCategory
     }
 }
--- SharingKnowledge.Services.Core/OpenCoursesService.cs
+++ SharingKnowledge.Services.Core/OpenCoursesService.cs
@@ -1,4 +1,5 @@
-﻿using SharingKnowledge.Services.Core.Interfaces;
+﻿using SharingKnowledge.Models;
+using SharingKnowledge.Services.Core.Interfaces;
 using SharingKnowledge.ViewModels.Courses;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,52 @@ namespace SharingKnowledge.Services.Core
 {
     public class OpenCoursesService : IOpenCoursesService
     {
+        public Task<bool> CategoryExistsAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateCourseAsync(OpenCoursesCreateInputModel inputModel, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCourseAsync(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditCourseAsync(int id, OpenCoursesCreateInputModel inputModel, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<OpenCoursesAllViewModel>> GetAllCoursesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<IEnumerable<OpenCoursesAllViewModel>> GetAllOpenCoursesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CourseCategory>> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OpenCoursesDetailsViewModel?> GetCourseDetailsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OpenCoursesDeleteViewModel?> GetCourseForDeleteAsync(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OpenCoursesCreateInputModel?> GetCourseForEditAsync(int id, string userId)
         {
             throw new NotImplementedException();
         }
