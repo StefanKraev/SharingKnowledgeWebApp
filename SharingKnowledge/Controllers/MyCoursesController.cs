03c62399537ecc6c39f93be248e1635545936edb
StefanKraev <stefanemilovkraev@abv.bg>
2026-03-31 21:54:31 +0300

    Created new controller for the enrolled courses of each student (view enrolled courses, un/enroll course)

---
+++ SharingKnowledge/Controllers/MyCoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharingKnowledge.Controllers
+{
+    public class MyCoursesController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
