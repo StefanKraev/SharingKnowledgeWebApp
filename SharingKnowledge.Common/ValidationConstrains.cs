291d9f0c256935cfc5130cb25dc00dedd6b4e904
StefanKraev <stefanemilovkraev@abv.bg>
2026-04-03 00:17:02 +0300

    Created Book entity.

--- SharingKnowledge.Common/ValidationConstrains.cs
+++ SharingKnowledge.Common/ValidationConstrains.cs
@@ -17,5 +17,14 @@
         /*Student property validations*/
         public const string StudentFNRegularExpression = @"^\d{1}MI\d{7}$";
         public const int StudentFNMaxLen = 15;
+
+        /*Book property validations*/
+        public const int BookTitleMinLength = 2;
+        public const int BookTitleMaxLength = 60;
+        public const int BookAuthorMinLength = 1;
+        public const int BookAuthorMaxLength = 50;
+        public const int BookDescriptionMinLength = 10;
+        public const int BookDescriptionMaxLength = 200;
+        public const int BookImageUrlMaxLength = 2048;
     }
 }
---
+++ SharingKnowledge.Data.Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using SharingKnowledge.Models;
+using static SharingKnowledge.Common.ValidationConstrains;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharingKnowledge.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(BookTitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(BookAuthorMaxLength)]
+        public string AuthorName { get; set; } = null!; // Could be a separate Author entity, but for simplicity, we store the name directly in the Book entity.
+
+        [Required]
+        [MaxLength(BookDescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [MaxLength(BookImageUrlMaxLength)]
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CourseCategory))]
+        public int CategoryId { get; set; }
+
+        public virtual CourseCategory Category { get; set; } = null!;
+
+        public virtual ICollection<Book> UserBooks { get; set; } = new List<Book>();
+    }
+}
