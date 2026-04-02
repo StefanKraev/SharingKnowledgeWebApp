09da7e921fb4070305716a7ea5d41414727a34b4
StefanKraev <stefanemilovkraev@abv.bg>
2026-04-02 22:07:50 +0300

    Added seed data to both appuser and student for tests.

---
+++ SharingKnowledge.Data/Configurations/ApplicationUserEntityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SharingKnowledge.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharingKnowledge.Data.Configurations
+{
+    public class UserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            string ghostAdminUserId = "007e37ed-27e5-43cb-a8b7-a3b14d054f45";
+
+            var adminUser = new ApplicationUser
+            {
+                Id = ghostAdminUserId,
+                UserName = "GhostAdmin",
+                NormalizedUserName = "GHOSTADMIN",
+                Email = "admin@sharingknowledge.com",
+                NormalizedEmail = "ADMIN@SHARINGKNOWLEDGE.COM",
+                EmailConfirmed = true,
+                SecurityStamp = "3235650d-6e47-49f3-9d0a-04664879201a",
+                ConcurrencyStamp = "86778f79-246e-4861-8271-6c589679199c",
+                PasswordHash = "AQAAAAIAAYagAAAAEGVx7nHYW18d6vEjqAitwoxLyms6pEPIbLVGX7rivzEBJMpHtztkFthMsfbEaXgWvQ=="
+            };
+
+            builder.HasData(adminUser);
+        }
+    }
+}
