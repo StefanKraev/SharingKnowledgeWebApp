86540adee6ea39dc44b8231502a6ac5a8563ff2a
StefanKraev <stefanemilovkraev@abv.bg>
2026-04-03 17:51:14 +0300

    Created DataBaseSeedConfiguration for seeding roles

---
+++ SharingKnowledge.Data/Configurations/DataBaseSeeder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SharingKnowledge.Data.Configurations
+{
+    public static class DataBaseSeeder
+    {
+        public static void SeedRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Admin", "Student", "VerifiedTeacher" };
+            foreach (var roleName in roleNames)
+            {
+                var roleExists = roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult();
+                if (!roleExists)
+                {
+                    var result = roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                    if(!result.Succeeded)
+                    {
+                        throw new Exception($"Failed to create role: {roleName}");
+                    }
+                }
+            }
+        }
+    }
+}
