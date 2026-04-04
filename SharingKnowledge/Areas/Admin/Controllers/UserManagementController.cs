f9c360f15f39a681208118562e32b2469a7a458d
StefanKraev <stefanemilovkraev@abv.bg>
2026-04-05 00:11:53 +0300

    Created Folder Admin for area. Created empty Home and UserManagement Controllers plus Views

---
+++ SharingKnowledge/Areas/Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharingKnowledge.Areas.Admin.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
