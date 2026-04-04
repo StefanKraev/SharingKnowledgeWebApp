08aee1cf39c783381c6c06392fdd2dc5e65e969e
StefanKraev <stefanemilovkraev@abv.bg>
2026-04-05 00:53:48 +0300

    Added general view for admin (dashboard). Added view for displaying users in the application.

--- SharingKnowledge/Areas/Admin/Controllers/UserManagementController.cs
+++ SharingKnowledge/Areas/Admin/Controllers/UserManagementController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SharingKnowledge.Areas.Admin.ViewModels;
+using SharingKnowledge.Data.Models;
 
 namespace SharingKnowledge.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class UserManagementController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UserManagementController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = userManager
+                .Users
+                .ToList();
+
+            var userViewModels = new List<UserViewModel>();
+
+            foreach (var user in users)
+            {
+                var roles = await userManager.GetRolesAsync(user);
+                userViewModels.Add(new UserViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Roles = roles.ToList()
+                });
+            }
+
+            return View(userViewModels);
         }
     }
 }
---
+++ SharingKnowledge/Areas/Admin/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SharingKnowledge.Areas.Admin.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
