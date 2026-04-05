84ab8dccbb16f30a12b988271898e569ee798d1b
StefanKraev <stefanemilovkraev@abv.bg>
2026-04-05 19:24:16 +0300

    Created new project for unit testing.

---
+++ SharingKnowledge.Tests/OpenCourseServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharingKnowledge.Tests
+{
+    public class OpenCourseServiceTests
+    {
+    }
+}
---
+++ SharingKnowledge.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace SharingKnowledge.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void DummyTest()
+        {
+            Assert.Pass("Just testing");
+        }
+    }
+}
