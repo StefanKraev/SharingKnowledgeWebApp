056d8af99ded2b3a4cf72f552e5cf9c5c1eb7d27
StefanKraev <stefanemilovkraev@abv.bg>
2026-04-06 13:06:50 +0300

    Made tests for creating course functionality

--- SharingKnowledge.Tests/OpenCourseServiceTests.cs
+++ SharingKnowledge.Tests/OpenCourseServiceTests.cs
@@ -2,14 +2,27 @@
 using SharingKnowledge.Data.Models;
 using SharingKnowledge.Data.Repository.Contracts;
 using SharingKnowledge.Models;
+using SharingKnowledge.Services.Common.Exceptions;
 using SharingKnowledge.Services.Core;
 using SharingKnowledge.ViewModels.Courses;
 using Xunit;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 namespace SharingKnowledge.Tests
 {
     public class OpenCourseServiceTests
     {
+        private readonly Mock<ICourseRepository> mockCourseRepo;
+        private readonly Mock<IStudentRepository> mockStudentRepo;
+        private readonly OpenCoursesService service;
+
+        public OpenCourseServiceTests()
+        {
+            this.mockCourseRepo = new Mock<ICourseRepository>();
+            this.mockStudentRepo = new Mock<IStudentRepository>();
+            this.service = new OpenCoursesService(mockCourseRepo.Object, mockStudentRepo.Object);
+        }
+
         [Fact]
         public async Task GetCourseDetailsAsync_whenCourseExists_ReturnsTrue()
         {
@@ -28,14 +41,9 @@ namespace SharingKnowledge.Tests
                 }
             };
 
-            var mockStudentRepository = new Mock<IStudentRepository>();
-
-            var mockCourseRepository = new Mock<ICourseRepository>();
-                
-            mockCourseRepository.Setup(repo => repo.GetOpenCourseByIdAsync(1))
+            mockCourseRepo.Setup(repo => repo.GetOpenCourseByIdAsync(1))
                 .ReturnsAsync(openCourse);
-
-            var service = new OpenCoursesService(mockCourseRepository.Object, mockStudentRepository.Object);
+       
 
             //Act
             var result = await service.GetCourseDetailsAsync(1);
@@ -50,20 +58,83 @@ namespace SharingKnowledge.Tests
         public async Task GetCourseDetailsAsync_whenCourseDoesNotExists_ReturnsFalse()
         {
             //Arrange
-            var mockStudentRepository = new Mock<IStudentRepository>();
-
-            var mockCourseRepository = new Mock<ICourseRepository>();
-
-            mockCourseRepository.Setup(repo => repo.GetOpenCourseByIdAsync(It.IsAny<int>()))
+            mockCourseRepo.Setup(repo => repo.GetOpenCourseByIdAsync(It.IsAny<int>()))
                 .ReturnsAsync((OpenCourse?)null);
-
-            var service = new OpenCoursesService(mockCourseRepository.Object, mockStudentRepository.Object);
 
             //Act
             var result = await service.GetCourseDetailsAsync(99999);
 
             //Assert
             Xunit.Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task CreateCourseAsync_WhenStudentExists_ShouldCallAddCourse()
+        {
+            // Arrange
+            var userId = "user-123";
+            var student = new Student { Id = 10, UserId = userId };
+            var inputModel = new OpenCoursesCreateInputModel
+            {
+                Title = "New Course",
+                Description = "Description",
+                StartDate = DateTime.Now,
+                CategoryId = 1
+            };
+
+            mockStudentRepo.Setup(r => r.GetStudentByIdAsync(userId))
+                .ReturnsAsync(student);
+
+            mockCourseRepo.Setup(r => r.AddOpenCourseAsync(It.IsAny<OpenCourse>()))
+                .ReturnsAsync(true); // As we are testing only the existence of the user
+
+            // Act
+            await service.CreateCourseAsync(inputModel, userId);
+
+            // Assert
+            mockCourseRepo.Verify(r => r.AddOpenCourseAsync(It.Is<OpenCourse>(c =>
+                c.Title == inputModel.Title &&
+                c.CreatorId == student.Id)), Times.Once);
+        }
+
+        [Fact]
+        public async Task CreateCourseAsync_WhenStudentDoesNotExists_ShouldCallAddCourse()
+        {
+            // Arrange
+
+            mockStudentRepo.Setup(r => r.GetStudentByIdAsync("dummyId"))
+                .ReturnsAsync((Student?)null);
+
+            var inputModel = new OpenCoursesCreateInputModel();
+
+            // Assert/Act
+            await Xunit.Assert.ThrowsAsync<Exception>(() =>
+             service.CreateCourseAsync(inputModel, "dummyId"));
+        }
+
+        [Fact]
+        public async Task CreateCourseAsync_WhenRepositoryFails_ShouldThrowSpecificException()
+        {
+            // Arrange
+            var userId = "user-123";
+            var student = new Student { Id = 10, UserId = userId }; //Valid Student
+            var inputModel = new OpenCoursesCreateInputModel
+            {
+                Title = "New Course",
+                Description = "Description",
+                StartDate = DateTime.Now,
+                CategoryId = 1
+            };
+
+            mockStudentRepo.Setup(r => r.GetStudentByIdAsync(userId))
+                .ReturnsAsync(student);
+
+            mockCourseRepo.Setup(r => r.AddOpenCourseAsync(It.IsAny<OpenCourse>()))
+                .ReturnsAsync(false);
+
+            // Act/Assert
+            await Xunit.Assert.ThrowsAsync<OpenCourseCreationFaliureExcpetion>(() =>
+             service.CreateCourseAsync(inputModel, userId));
         }
     }
 }
